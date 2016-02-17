2577dc87327472a74b273f8b32af43c9e7e33af8
Raymond Ho <rho002@ucr.edu>
2016-02-17 15:32:50 -0800

    Don't have android cable, but compiles. Finished listing, playback when chosen, and leaving the app. Now working on shuffling.

--- MusicPlayer/app/src/main/java/com/example/horay/musicplayer/MusicService.java
+++ MusicPlayer/app/src/main/java/com/example/horay/musicplayer/MusicService.java
@@ -2,15 +2,111 @@ package com.example.horay.musicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
 import android.os.IBinder;
 
 /**
  * Created by HoRay on 2/12/2016.
  */
-public class MusicService extends Service{
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+
+    //media player
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
+
+    private final IBinder musicBind = new MusicBinder();
+
+    public void onCreate(){
+        //create the service
+        super.onCreate();
+        //initialize position
+        songPosn=0;
+        //create player
+        player = new MediaPlayer();
+
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer(){
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
     @Override
-    public IBinder onBind(Intent arg0) {
-        // TODO Auto-generated method stub
-        return null;
+    public IBinder onBind(Intent intent) {
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    public void playSong(){
+        player.reset();
+
+        //get song
+        Song playSong = songs.get(songPosn);
+        //get id
+        long currSong = playSong.getID();
+        //set uri
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        //start playback
+        mp.start();
+    }
+
+    public void setSong(int songIndex){
+        songPosn=songIndex;
+    }
+
+    public void onCompletion(MediaPlayer mp) {
+    }
+    public boolean onError(MediaPlayer mp, int a, int b) {
+        return false;
     }
 }
